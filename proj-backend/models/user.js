3fc9fcc0cfd260a92c4e4a60cb3a2052905886f0
akki <akshat@insync.ai>
2020-07-02 20:20:52 +0530

    add encrption to password

--- proj-backend/models/user.js
+++ proj-backend/models/user.js
@@ -23,9 +23,9 @@ const userSchema = new mongoose.Schema({
     trim: true,
   },
   //TODO: come back
-  password: {
+  encry_password: {
     type: String,
-    trim: true,
+    required: true,
   },
   salt: String,
   role: {
@@ -37,5 +37,19 @@ const userSchema = new mongoose.Schema({
     default: [],
   },
 });
+
+userSchema.method = {
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      return crypto
+        .createHmax("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
